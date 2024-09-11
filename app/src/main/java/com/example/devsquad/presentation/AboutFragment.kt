10488a3b13e021ebfac62199d53eaa886a7eced2
YoussefAshraf114 <youssef.ashraf012883@gmail.com>
2024-09-12 02:46:53 +0300

    Adding Initial Layout for Home and About Fragments

---
+++ app/src/main/java/com/example/devsquad/presentation/AboutFragment.kt
@@ -0,0 +1,37 @@
+package com.example.devsquad.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.devsquad.R
+import com.example.devsquad.databinding.FragmentAboutBinding
+
+
+class AboutFragment : Fragment() {
+
+    val binding: FragmentAboutBinding by lazy {
+        FragmentAboutBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding.english.setOnClickListener {
+            Toast.makeText(requireContext(), "Coming soon", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.nightModeSwitch.setOnClickListener {
+            Toast.makeText(requireContext(), "Coming soon", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
+    }
+
+}
