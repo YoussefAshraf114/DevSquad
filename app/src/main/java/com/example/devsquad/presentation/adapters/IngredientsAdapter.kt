1e7f6ccf5f621c4cb1fa2c1d26f046abc6c6a2fd
Amr <amr.must232@gmail.com>
2024-09-14 17:27:29 +0300

    solving some errors

--- app/src/main/java/com/example/devsquad/presentation/adapters/IngredientsAdapter.kt
+++ app/src/main/java/com/example/devsquad/presentation/adapters/IngredientsAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.devsquad.R
 
 class IngredientsAdapter(private val ingredients: List<Ingredient>) :
     RecyclerView.Adapter<IngredientsAdapter.IngredientViewHolder>() {
